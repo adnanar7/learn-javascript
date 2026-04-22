ab81ae1d1f2e9dfa6f0a8a82af507d1842dd9c9f
Adnanar7 <adnanar0007@gmail.com>
2026-04-23 00:28:41 +0500

    hehe 2

--- 1. Build a Passenger Counter App/10. Write your first function/index.js
+++ 1. Build a Passenger Counter App/10. Write your first function/index.js
@@ -1,3 +1,8 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
+function you_decide_the_name() {
+    console.log(42)
+}
+
+you_decide_the_name()
--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -4,3 +4,9 @@ let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
 
+function lap_sum() {
+    sum = lap1 + lap2 + lap3
+    return sum
+}
+
+console.log(lap_sum())
